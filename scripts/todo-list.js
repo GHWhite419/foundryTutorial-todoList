1a9925f4d397acb190986b3afc1d482f288928af
Galen White <galenhwhite419@gmail.com>
2024-04-11 14:53:04 -0400

    Created ToDoListData class and a collection of methods to allow for Creating, Reading, Updating, and Deleting ToDo data.

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,13 +1,13 @@
 class ToDoList {
-    static ID = 'todo-list'
+  static ID = "todo-list";
 
-    static FLAGS = {
-        TODOS: 'todos'
-    }
+  static FLAGS = {
+    TODOS: "todos",
+  };
 
-    static TEMPLATES = {
-        TODOLIST: `modules/${this.ID}/templates/todo-list.hbs`
-    }
+  static TEMPLATES = {
+    TODOLIST: `modules/${this.ID}/templates/todo-list.hbs`,
+  };
 }
 
 /**
@@ -20,5 +20,65 @@ class ToDoList {
  */
 
 class ToDoListData {
-    
+  static getToDosForUser(userId) {
+    return game.users.get(userId)?.getFlag(ToDoList.ID, ToDoList.FLAGS.TODOS);
+  }
+
+  static createToDo(userId, toDoData) {
+    const newToDo = {
+      isDone: false,
+      ...toDoData,
+      id: foundry.utils.randomID(16),
+      userId,
+    };
+
+    const newToDos = {
+      [newToDo.id]: newToDo,
+    };
+
+    return game.users
+      .get(userId)
+      ?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, newToDos);
+  }
+
+  static get allToDos() {
+    const allToDos = game.users.reduce((accumulator, user) => {
+      const userToDos = this.getToDosForUser(user.id);
+
+      return {
+        ...accumulator,
+        ...userToDos,
+      };
+    }, {});
+
+    return allToDos;
+  }
+
+  static updateToDo(toDoId, updateData) {
+    const relevantToDo = this.allToDos[toDoId];
+
+    const update = {
+      [toDoId]: updateData,
+    };
+
+    return game.users
+      .get(relevantToDo.userId)
+      ?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, update);
+  }
+
+  static deleteToDo(toDoId) {
+    const relevantToDo = this.allToDos[toDoId];
+
+    const keyDeletion = {
+      [`-=${toDoId}`]: null,
+    };
+
+    return game.users
+      .get(relevantToDo.userId)
+      ?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, keyDeletion);
+  }
+
+  static updateUserToDos(userId, updateData) {
+    return game.users.get(userId)?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, updateData)
+  }
 }
