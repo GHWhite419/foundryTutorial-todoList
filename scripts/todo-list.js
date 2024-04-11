f7f0a17028f800082196b3aba4564880177f0910
Galen White <galenhwhite419@gmail.com>
2024-04-11 13:46:32 -0400

    Continued setup by defining ToDoList class and a ToDo typedef

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,1 +1,24 @@
-console.log('todo-list | Hello World!')
+class ToDoList {
+    static ID = 'todo-list'
+
+    static FLAGS = {
+        TODOS: 'todos'
+    }
+
+    static TEMPLATES = {
+        TODOLIST: `modules/${this.ID}/templates/todo-list.hbs`
+    }
+}
+
+/**
+ * A single ToDo in our list of ToDos.
+ * @typedef {Object} ToDo
+ * @property {string} id - A unique ID to identify this ToDo.
+ * @property {string} label - The text of the ToDo.
+ * @property {boolean} isDone - Marks whether the ToDo is done.
+ * @property {string} userId - The user who owns this ToDo.
+ */
+
+class ToDoListData {
+    
+}
