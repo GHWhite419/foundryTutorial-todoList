4415f9b30f32d8e8b3094fb9c30912f39cb58609
Galen White <galenhwhite419@gmail.com>
2024-04-16 21:28:25 -0400

    Created commentary on ToDoListData class to help solidify my understanding.

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -21,64 +21,94 @@ class ToDoList {
 
 class ToDoListData {
   static getToDosForUser(userId) {
+    // To utilize these todo methods, we begin by targeting the given user's ID and passing that in as an argument.
+    // In this first method - getToDosForUser - we simply pass that user ID in and target it to get the given user's todo flags.
     return game.users.get(userId)?.getFlag(ToDoList.ID, ToDoList.FLAGS.TODOS);
   }
 
   static createToDo(userId, toDoData) {
+    // In createToDo, we first pass in the userId as above for one parameter, and then the toDoData we're writing as another.
+    // The toDoData is passed into this newToDo object to create a new flag.
     const newToDo = {
       isDone: false,
       ...toDoData,
       id: foundry.utils.randomID(16),
       userId,
+      // isDone will act as our checkbox, marked false - incomplete - by default. Wouldn't make sense to create a todo that's already done now, would it?
+      // toDoData is accepted above as an argument for this method and is then spread into the newToDo object. Since toDoData is an object like newToDo, We use spread to ensure all previous information is merged with the structure of the newToDo object instead of overwriting it.
+      // For id, we use a built-in foundry utility to generate a random string, the argument indicates it will be 16 character's in length. When we need to update or delete said to do, we can target this id.
+      // And finally, userId is passed in from the parameter above to indicate which user owns the newToDo object. We use shorthand here as the value is already defined when the userId is passed in.
     };
 
     const newToDos = {
       [newToDo.id]: newToDo,
+      // In order to set this newToDo as a new flag, we need to convert the entire newToDo object into a property of a larger object. We'll do this by defining the todo's id as the key, and the entire newToDo object as the value. The reason we do this is...
     };
 
-    return game.users
-      .get(userId)
-      ?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, newToDos);
+    return (
+      game.users
+        // ...because of the way the setFlag method is set up within Foundry. First of all, the TODOS flags is an object that is similar to the newToDos object, rather than the newToDo object. There are several properties that follow the format of key = id, value = todo object. The way setFlag works is that when we set a newFlag, it merges that flag with the flags already in there instead of replacing it. However, if we set newToDo as the flag (instead of newToDos), the TODOS flags would conform to that structure, rather than its current, and would in effect overwrite the entire TODOS flags with the data from only a single to-do.
+        // Tl;dr we pass in "newToDos" instead of "newToDo" to merge new data with old data and avoid overwriting.
+        .get(userId)
+        ?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, newToDos)
+    );
   }
 
   static get allToDos() {
     const allToDos = game.users.reduce((accumulator, user) => {
+      // The accumulator argument acts as the progressive collection of user todos, beginning as an empty object.
+      // The user argument identifies a particular user as the reducer loops through the game.users collection, and is used to save those todos in a variable.
       const userToDos = this.getToDosForUser(user.id);
+      // userToDos stores the todos for the user currently targeted by the reducer.
 
       return {
+        // Using spread operators, we first make sure the currently collected information is returned and therefore saved from the accumulator.
+        // We then spread in the new userToDos, growing the accumulator before iterating for the next user.
         ...accumulator,
         ...userToDos,
       };
     }, {});
+    // The empty object argument indicates the starting state of our return.
 
     return allToDos;
   }
 
   static updateToDo(toDoId, updateData) {
+    // We first capture our targeted todo in relevantToDo, which references our allToDos object using the toDoId argument as the key.
+    // By creating the allToDos method, we've made it a lot easier to target our desired todo for update by removing the need to loop through users within the same method.
     const relevantToDo = this.allToDos[toDoId];
 
+    // Next we prepare a new object to merge in with existing data, using toDoId as the targeted key in our existing flag, and passing in our updateData argument as the new data.
     const update = {
       [toDoId]: updateData,
     };
 
+    // We finish by using the setFlag data (passing in the update object as our argument) to merge in the updated data with the existing data, replacing any conflicts - similar to how the createToDo method works.
     return game.users
       .get(relevantToDo.userId)
       ?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, update);
   }
 
   static deleteToDo(toDoId) {
+    // Only needing one parameter here, we pass in our targeted todo's id and save it to a variable, once again using the allToDos object to make our lives easier.
     const relevantToDo = this.allToDos[toDoId];
 
+    // We then create our keyDeletion object, using Foundry's specific syntax for deleting flags.
+    // Instead of targeting our todo directly by passing in the id as the key, we instead create a new object, with a dynamic key that matches our target todo's id. Along with the special syntax and the template literal, we set the value as null. This tells Foundry that we want to delete the flag, ratherr than update it with a value of null.
     const keyDeletion = {
       [`-=${toDoId}`]: null,
     };
 
+    // Finally we set the flag using our special object, and voila: the todo is deleted.
     return game.users
       .get(relevantToDo.userId)
       ?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, keyDeletion);
   }
 
   static updateUserToDos(userId, updateData) {
-    return game.users.get(userId)?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, updateData)
+    // This simple method behaves similar to updateToDo, but targets userId instead of toDoId, allowing us to pass in updateData for multiple todos at once.
+    return game.users
+      .get(userId)
+      ?.setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, updateData);
   }
 }
